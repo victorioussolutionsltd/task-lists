1b184cc5df3628ea11f5185fe115529bd447ae61
Piotr Malecki <victorious.solutions@icloud.com>
2022-11-14 16:02:08 +0000

    Add collection function helpers.

---
+++ tasks-app/src/helpers/intersection.tsx
@@ -0,0 +1,14 @@
+import { ListItemType } from '../types'
+
+export function not(a: readonly ListItemType[], b: readonly ListItemType[]) {
+  return a.filter((value) => b.indexOf(value) === -1);
+}
+export function intersection(a: readonly ListItemType[], b: readonly ListItemType[]) {
+  return a.filter((value) => b.indexOf(value) !== -1);
+}
+export function union(a: readonly ListItemType[], b: readonly ListItemType[]) {
+  return [...a, ...not(b, a)];
+}
+
+export const numberOfChecked = (checked: readonly ListItemType[], items: readonly ListItemType[]) =>
+intersection(checked, items).length;
