485625645532afb0c3c655f9d18de23c5480fe04
Piotr Malecki <victorious.solutions@icloud.com>
2022-11-15 15:07:31 +0000

    Add tests for helpers.

--- tasks-app/src/helpers/intersection.test.tsx
+++ tasks-app/src/helpers/intersection.test.tsx
@@ -1,5 +1,27 @@
-import { not } from './intersection';
+import { not, intersection, union, numberOfChecked } from './intersection';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(not(1, 2)).toBe(true);
+describe('Array Helpers', () => {
+  const listA = [{value: 1, description: "hello"},{value: 2, description: "hello"}] ;
+  const listB = [{value: 1, description: "test"},{value: 2, description: "test"}] ;
+  
+  test('returns elements not belonging to second array', () => {
+    expect(not(listA, listB)).toStrictEqual([{"description": "hello", "value": 1}, {"description": "hello", "value": 2}]);
+    expect(not(listA, listA)).toStrictEqual([]);
+  });
+
+  test('returns intersection of two arrays', () => {
+    expect(intersection(listA, listB)).toStrictEqual([]);
+    expect(intersection(listA, listA)).toStrictEqual([{value: 1, description: "hello"},{value: 2, description: "hello"}]);
+  })
+
+  test('returns union of two arrays', () => {
+    expect(union(listA, listB)).toStrictEqual([{value: 1, description: "hello"},{value: 2, description: "hello"}, {value: 1, description: "test"},{value: 2, description: "test"}]);
+    expect(union(listA, listA)).toStrictEqual([{value: 1, description: "hello"},{value: 2, description: "hello"}]);
+  })
+
+  test('returns number of checked items of two arrays', () => {
+    expect(numberOfChecked(listA, listB)).toBe(0);
+    expect(numberOfChecked(listA, listA)).toBe(2);
+  })
+  
 });
