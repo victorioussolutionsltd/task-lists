90333983b8a3c9a3e22c7de9b80d921333c2fd61
Piotr Malecki <victorious.solutions@icloud.com>
2022-11-15 16:27:13 +0000

    Move state logic into context.

---
+++ tasks-app/src/context/TasksContext.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useMemo} from 'react';
+import { intersection, not } from '../helpers/intersection';
+import { ListItemType, TasksContextType } from '../types';
+
+export const TasksContext = React.createContext<TasksContextType | null>(null);
+
+function TasksProvider ({ children } : {children: React.ReactNode})  {
+  const [checked, setChecked] = useState< ListItemType[]>([]);
+  const [left, setLeft] = useState< ListItemType[]>([]);
+  const [right, setRight] = useState< ListItemType[]>([]);
+
+  const leftChecked = intersection(checked, left);
+  const rightChecked = intersection(checked, right);
+
+  const addNewItem = (newItem : ListItemType) => {
+    if (newItem) {
+      const newLeft = [...left, newItem];
+      setLeft(newLeft);
+    }
+  };
+
+  const handleCheckedRight = () => {
+    setRight(right.concat(leftChecked));
+    setLeft(not(left, leftChecked));
+    setChecked(not(checked, leftChecked));
+  };
+
+  const handleCheckedLeft = () => {
+    setLeft(left.concat(rightChecked));
+    setRight(not(right, rightChecked));
+    setChecked(not(checked, rightChecked));
+  };
+
+  const globalContextValue = useMemo(
+    () => ({
+      addNewItem,
+        checked,
+        setChecked,
+        handleCheckedRight,
+        handleCheckedLeft,
+        right,
+        left,
+        rightChecked,
+        leftChecked
+    }),
+    [addNewItem,
+      checked,
+      setChecked,
+      handleCheckedRight,
+      handleCheckedLeft,
+      right,
+      left,
+      rightChecked,
+      leftChecked]
+  );
+
+  return (
+    <TasksContext.Provider
+      value={globalContextValue}
+    >
+      {children}
+    </TasksContext.Provider>
+  );
+};
+
+export default TasksProvider;
--- tasks-app/src/types.tsx
+++ tasks-app/src/types.tsx
@@ -2,3 +2,16 @@ export type ListItemType = {
   value: number;
   description: string;
 };
+
+
+export type TasksContextType = {
+  addNewItem: (item: ListItemType) => void;
+  checked: ListItemType[];
+  setChecked: (items: ListItemType[]) => void;
+  handleCheckedRight: () => void;
+  handleCheckedLeft: () => void;
+  right:  ListItemType[];
+  left:  ListItemType[];
+  rightChecked:  ListItemType[];
+  leftChecked:  ListItemType[];
+};
