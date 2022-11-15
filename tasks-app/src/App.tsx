846d56fa79df95d2c84f83ed3885802fbecc24c4
Piotr Malecki <victorious.solutions@icloud.com>
2022-11-15 15:08:37 +0000

    Add input validation. Change the layout.

--- tasks-app/src/App.tsx
+++ tasks-app/src/App.tsx
@@ -1,17 +1,37 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { Grid, Button } from '@mui/material';
+import { Button } from '@mui/material';
+import styled from 'styled-components'
 import TransferList from './TransferList';
 import { ListItemType } from './types';
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center
+`;
+
+const MainWrapper = styled.div`
+  display: flex,
+  height: 90%,
+  flex-direction: column,
+  align-content: space-around
+`
 
 function App() {
   const [description, setDescription] = useState('Task description');
   const [item, setItem] = useState<ListItemType | null>(null);
+  const [helperText, setHelperText] = useState('')
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if(event.target.value.length > 0) { setHelperText('') }
     setDescription(event.target.value);
   };
 
   const addNewItem = () => {
+    if(description === ''){
+      setHelperText('Provide the description')
+      return;
+    }
+
     if (item) {
       setItem({ value: item.value + 1, description });
     } else {
@@ -20,23 +40,25 @@ function App() {
   };
 
   return (
-    <Grid container spacing={1} justifyContent="center" alignItems="center">
-      <Grid item>
-        <TextField
-          id="filled-basic"
-          label="Filled"
-          variant="filled"
-          value={description}
-          onChange={handleChange}
-        />
-        <Button variant="text" onClick={addNewItem}>
-          Add Task
-        </Button>
-      </Grid>
-      <Grid>
+    <MainWrapper>
+      <InputWrapper>
+          <TextField
+            id="filled-basic"
+            label="Filled"
+            variant="filled"
+            value={description}
+            onChange={handleChange}
+            helperText={helperText}
+          />
+          <Button variant="text" onClick={addNewItem}>
+            Add Task
+          </Button>
+      </InputWrapper>
+      <br />
+      <br />
+
         <TransferList newItem={item} />
-      </Grid>
-    </Grid>
+    </MainWrapper>
   );
 }
 
