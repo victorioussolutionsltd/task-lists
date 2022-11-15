ad35b324fdb14b80cb0ee45dc57453339b92b147
Piotr Malecki <victorious.solutions@icloud.com>
2022-11-15 13:35:18 +0000

    Add linter

---
+++ tasks-app/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+};
